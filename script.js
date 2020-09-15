9cce7fda5fdf92ab5aba1df2d162b7bb6044ef95
Colby Scarbrough <cws18472@gmail.com>
2020-09-15 15:38:27 -0400

    added stub main function and comment description in js

---
+++ script.js
@@ -0,0 +1,12 @@
+/**********************************************
+
+This script is not for the actual Carbon calculator
+script works with functionality of the web application
+
+UI of website
+
+***********************************************/
+
+function main() {
+    return null
+}
